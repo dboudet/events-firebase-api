450d65eca06c8768b5b40d9111ced660af9137ab
Dan Boudet <dboudet04@gmail.com>
2021-07-14 00:05:02 -0400

    created requests to get event by id, update event, delete event, search by name

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-const { getEvents, createEvent } = require('./src/events')
+const { getEvents, createEvent, getSingleEvent, deleteEvent, updateEvent, findEventByQuery } = require('./src/events')
 
+app.get('/events/search', findEventByQuery)
+app.get('/events/:eventId', getSingleEvent)
 app.get('/events', getEvents)
 app.get('/',(req,res) => {
     res.send('This is our first get.')
@@ -12,5 +15,8 @@ app.get('/',(req,res) => {
 
 app.post('/events', createEvent )
 
+app.patch('/events/:eventId', updateEvent)
+
+app.delete('/events/:eventId', deleteEvent)
 
 app.listen(5000, () => console.log("Listening to http://localhost:5000"))
--- src/events.js
+++ src/events.js
@@ -1,5 +1,6 @@
+const { req, res } = require('express')
 const admin = require('firebase-admin')
-const credentials = require("../credentials.json")
+const credentials = require('../credentials.json')
 
 function connectDb() {
     if(!admin.apps.length){
@@ -14,9 +15,18 @@ exports.getEvents = (req,res) => {
     const db = connectDb()
     db.collection('events')
         .get()
-        .then(allEvents => allEvents.docs.map(doc => console.log(doc.data())))
-        res.send("Got all events.")
-        .catch(err => console.log(err))
+        .then(eventsCollection => {
+            let allEvents = eventsCollection.docs.map(eventDoc => {
+                let event = eventDoc.data()
+                event.id = eventDoc.id
+                return event
+            })
+            res.send(allEvents)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).send(err)
+        })
 }
 
 //create a POST request - to add new events
@@ -24,29 +34,76 @@ exports.createEvent = (req,res) => {
     const db = connectDb()
     db.collection('events')
         .add(
-            {
-                "where": "Palm Beach Gardens",
-                "paying": "Jonathan",
-                "name": "Brunch at Twisted Trunk Brewing",
-                "time": "11:00 a.m.",
-                "date": "July 22, 2021"
+            {...req.body,
+                "drinks on":"Jonathan"
             }
         )
-        .then(singleEvent => res.status(200).send(`New event created with id: ${singleEvent.id}`))
+            .then(singleEvent => res.status(200).send(`New event created with id: ${singleEvent.id}`))
         .catch(err => res.status(500).send(err))
 }
 
-// create GET - ONE event request (by ID)
+// GET ONE event request (by ID)
 exports.getSingleEvent = (req,res) => {
     const db = connectDb()
-    db.collection('events')
-        .get()
-        .then()
-        .catch(err => res.send("Error getting event:"),err)
+    if(!req.params.eventId) {
+        res.status(404).send("No event specified.")
+        return
+    }
+    db.collection('events').doc(req.params.eventId).get()
+        .then( doc => {
+            let event = doc.data()
+            event.id = doc.id
+            res.send(event)
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).send("Error getting event."),err
+        })
 }
+
 // create DELETE request - to delete one event
+exports.deleteEvent = (req,res) => {
+    const db = connectDb()
+    if(!req.params.eventId) {
+        res.status(404).send("No event specified.")
+        return
+    }
+    // const { eventId } = req.params
+    // db.connection('events').doc(eventId).delete()
+    db.collection('events').doc(req.params.eventId).delete()
+        .then( res.status(203).send("Event successfully deleted."))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send("Error deleting event.",err)
+        })
+}
 
 // create PATCH request - to update one event
+exports.updateEvent = (req,res) => {
+    const db = connectDb()
+    const { eventId } = req.params
+    db.collection('events').doc(eventId).update(
+        {...req.body,
+            timestamp: admin.firestore.FieldValue.serverTimestamp(),
+            "drinks on":"Jonathan"
+        }
+        )
+        .then( () => res.status(201).send(`Event ${eventId} updated.`))
+        .catch( err => res.status(500).send(err))
+}
 
-// make a SEARCH request - to find one event
-// make it dynamic using body of request in Postman
+// make a SEARCH request - to find one event (by name)
+exports.findEventByQuery = (req,res) => {
+    const db = connectDb()
+    const { queryName } = req.query
+    db.collection('events').where('name','==', queryName).get()
+        .then( eventsCollection => {
+            const matches = eventsCollection.docs.map( eventDoc => {
+                let event = eventDoc.data()
+                event.id = eventDoc.id
+                return event
+            })
+            res.send(matches)
+        })
+        .catch(err => res.status(500).send(err))
+}
